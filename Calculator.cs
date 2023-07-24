4498d2d2906362b9d911b116063c91fd5ae838d3
NearIsSadd <nearissad@gmail.com>
2023-07-23 20:03:50 -0500

    just making some classes

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Calculator.Menu {
+
+    public partial class UserMenu {
+
+        public void displayMenu() {
+            Console.WriteLine("");
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Calculator.Menu;
+
+namespace Calculator {
+    class Main {
+        public static void main(string[] args) {
+
+        }
+    }
+}
